08c889b9d92a3ba691dd641dab48e5bbeb780cb0
Pratt SAVI <58483678+PrattSAVI@users.noreply.github.com>
2021-07-23 10:38:47 -0400

    New Logos

--- py/DataEditting.py
+++ py/DataEditting.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import geopandas as gpd
 import os
+#%%
 
 folder = r'C:\Users\csucuogl\Documents\GitHub\RAM\data'
 
@@ -49,8 +50,12 @@ df['Primary TEC'].unique()
 
 #%%
 
+which_one = 'Completed'
+path = r"C:\Users\csucuogl\Documents\GitHub\RAM\data\{}.geojson".format(which_one)
+df = gpd.read_file( path )
+
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'HABITAT FOR WATERBIRDS' , "STECIslands")
-df['Primary TEC'] = df['Primary TEC'].str.replace( 'COASTAL WETLANDS' , "STECCoastWet")
+df['Primary TEC'] = df['Primary TEC'].str.replace( 'COASTAL WETLANDS' , "STECWetland")
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'TRIBUTARY CONNECTIONS' , "STECTributary")
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'COASTAL AND MARITIME FORESTS' , "STECForests")
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'OYSTER REEFS' , "STECOyster")
@@ -62,7 +67,9 @@ df['Primary TEC'] = df['Primary TEC'].str.replace( 'ACQUISITION' , "STECAcquisit
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'FISH, CRABS AND LOBSTERS' , "STECAquaticHab")
 df['Primary TEC'] = df['Primary TEC'].str.replace( 'SEDIMENT COMINATION' , "STECSediment")
 
-df['Primary TEC'].unique()
+print( df['Primary TEC'].unique() )
+
+df.to_file( path , driver = "GeoJSON" , encoding ='utf-8')
 
 #%%
 
@@ -75,6 +82,59 @@ df.to_file(  os.path.join(folder , "Completed.geojson") , encoding ='utf-8',driv
 
 # %%
 
+pd.set_option('display.max_columns', None)
+which_one = "Retired"
+path = r"C:\Users\csucuogl\Documents\GitHub\RAM\data\{}.geojson".format(which_one)
+working = gpd.read_file( path )
+working.head()
 
+
+
+#%%
+
+for i in working.columns[ working.columns.str.contains("STEC") ]:
+    print(i)
+    print( working[i].unique() )
+
+#%%
+
+working['STECCoastWet'] = working['STECCoastWet'].replace('False',False)
+working['STECCoastWet'] = working['STECCoastWet'].replace('True',True)
+working['STECCoastWet'] = working['STECCoastWet'].replace('TRUE This is FWW so should be FALSE',False)
+working['STECCoastWet'] = working['STECCoastWet'].replace('TRUE ',True)
+
+working['STECCoastWet'].unique()
+
+
+# %%
+
+working['db'] = which_one
+# %%
+
+working.columns[ working.columns.str.contains("STEC") ]
+
+#working['STECWetland'] = (working['STECCoastWet'] | working['STECFreshWet']) 
+#working.loc[ (working['STECFreshWet'] == True) & (working['STECCoastWet'] == True)  , 'STECWetland' ] = True
+
+
+working['STECWetland'] = (working['STECCoastWet'] | working['STECFreshWet']) 
+
+working.loc[ (working['STECFreshWet'] == True) & (working['STECCoastWet'] == True)  , 'STECWetland' ] = True
+
+working[ ['STECWetland','STECCoastWet','STECFreshWet'] ].head(20)
+
+#%%
+
+working = working.drop( ['STECCoastWet','STECFreshWet'] , axis =1 )
+working.head()
+
+#%%
+
+working.to_file( path , driver = "GeoJSON" , encoding ='utf-8')
+# %%
+
+# %%
+
+working.columns[ working.columns.str.contains("STEC") ]
 
 # %%
